f0d12ff6e3450d961529fdbcd9577e1b65deb46d
Steve Hocking <steve@codemunki.es>
2022-12-18 22:24:07 +0000

    Initial basic cloudformation for static site

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { StaticSiteStack } from '../lib/static-site-stack';
+import console = require('console');
+
+console.log('Account: ' + process.env.CDK_DEFAULT_ACCOUNT);
+console.log('Region: ' + process.env.CDK_DEFAULT_REGION);
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
+new StaticSiteStack(app, 'StaticSiteStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+   * but a single synthesized template can be deployed anywhere. */  
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
